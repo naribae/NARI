47bbc51a4e03912d1c4461e83260095677abbfe8
naribae <nari_b@hanatour.com>
2020-10-23 14:38:37 +0900

    1

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import pandas as pd
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+retailDF = pd.read_excel(io='C:\\Users\\hana\\Desktop\\Online Retail.xlsx')
+print(retailDF.info())
+retailDF = retailDF[retailDF['Quantity']>0]
+retailDF = retailDF[retailDF['UnitPrice']>0]
+retailDF = retailDF[retailDF['CustomerID'].notnull()]
+print(retailDF.shape)
+print(retailDF.isnull().sum())
+print(retailDF['Country'].value_counts()[:5])
+retailDF = retailDF[retailDF['Country']=='United Kingdom']
+print(retailDF.shape)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+retailDF['sale_amount'] = retailDF['Quantity'] * retailDF['UnitPrice']
+retailDF['CustomerID'] = retailDF['CustomerID'].astype(int)
+print(retailDF['CustomerID'].value_counts().head())
+print(retailDF.groupby('CustomerID')['sale_amount'].sum().sort_values(ascending=False)[:5])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+aggregations = {
+    'InvoiceDate': 'max',
+    'InvoiceNo': 'count',
+    'sale_amount': 'sum'
+}
+cust_df = retailDF.groupby('CustomerID').agg(aggregations)
+cust_df = cust_df.rename(columns={'InvoiceDate':'Recency', 'InvoiceNo':'Frequency', 'sale_amount':'Monetary'})
+cust_df = cust_df.reset_index()
+cust_df['Recency'] = datetime.datetime(2021,10,2) - cust_df['Recency']
+cust_df['Recency'] = cust_df['Recency'].apply(lambda x:x.days+1)
+print(cust_df)
